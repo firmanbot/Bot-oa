4ab8317e363cc1ab5b3a80ccb18103278a55334e
firmanbot <33959757+firmanbot@users.noreply.github.com>
2017-12-27 07:09:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def handle_message(event):
         return 0
     elif "Idl " in event.message.text:
 	cctxt = event.message.text.replace("Idl ","")
-        text_message = TextSendMessage(text=cctct)
+        text_message = TextSendMessage(text=cctxt)
         line_bot_api.reply_message(event.reply_token, text_message)
         return 0
     if event.message.text == "Advent Stage":
